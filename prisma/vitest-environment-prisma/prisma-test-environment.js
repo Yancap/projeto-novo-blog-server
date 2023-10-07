6e6d023f967e71f45f5a128ade069fa632d27994
Yancap <yangabriel3001f@gmail.com>
2023-10-07 09:29:33 -0300

    refactor schema and enviroment variables

--- prisma/vitest-environment-prisma/prisma-test-environment.js
+++ prisma/vitest-environment-prisma/prisma-test-environment.js
@@ -4,8 +4,8 @@ require('dotenv/config')
 
 
 function generateDatabaseURL(uid){
-    if (!process.env.DATABASE_TEST_URL) throw new Error("Please provide a DATABASE_TEST_URL environment variable ")
-    const url = process.env.DATABASE_TEST_URL.toString("").replace("test.db", `test-${uid}.db`)
+    if (!process.env.DATABASE_URL) throw new Error("Please provide a DATABASE_URL environment variable ")
+    const url = `file:./test/test-${uid}.db`
     return url
 }
 
@@ -15,7 +15,7 @@ module.exports = {
     async setup(){
         const uid = randomUUID().substring(0,6);
         const databaseURL = generateDatabaseURL(uid)
-        process.env.DATABASE_TEST_URL = databaseURL
+        process.env.DATABASE_URL = databaseURL
         execSync('npx prisma migrate deploy')
         return {
             async teardown() {},
