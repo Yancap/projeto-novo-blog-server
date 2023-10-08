f2ab1d389a86efc25d1b09f9381fce3a7e299a48
Yancap <yangabriel3001f@gmail.com>
2023-10-08 13:19:42 -0300

    add PORT

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import { app } from './app';
 import { env } from './env';
+import 'dotenv/config'
+
+const port = process.env.PORT ? Number(process.env.PORT) : env.PORT
 
 app.listen({
     host: 'localhost',
-    port: env.PORT,
+    port: port,
 }).then(() => {
     console.log("HTTP server running on PORT", env.PORT);
 })
