b70fd9c9dcb17a817757e063ff9962f3e59475d0
Yancap <yangabriel3001f@gmail.com>
2023-10-08 14:51:52 -0300

    edit

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ import 'dotenv/config'
 const port = Number(process.env.PORT) || 3333
 
 app.listen({
-    host: 'localhost',
     port: port,
 }).then(() => {
     console.log("HTTP server running on PORT", port);
